1e667c051d29000b4f70a05089c2c3984dd1a869
Gabryss <gabriel.garcia@utbm.fr>
2021-03-25 23:43:33 +0100

    Add a new middleware (django middleware)

--- API/settings.py
+++ API/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'API.urls'
