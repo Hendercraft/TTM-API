6ac67e5ed65c66e42adc4cc858f1fc29f9a18208
Gabryss <gabriel.garcia@utbm.fr>
2021-05-05 22:46:43 +0200

    Minor changes

--- database/urls.py
+++ database/urls.py
@@ -3,18 +3,6 @@ from rest_framework import routers
 from rest_framework_simplejwt import views as jwt_views
 from database import views
 
-
-# router = routers.DefaultRouter()
-
-# router.register('dates', views.DateViewSet)
-# router.register('qualities', views.QualityViewSet)
-# router.register('sourceType', views.SourceTypeViewSet)
-# router.register('authors', views.AuthorViewSet)
-# router.register('contents', views.ContentViewSet)
-# router.register('urls', views.UrlViewSet)
-# router.register('sources', views.SourceViewSet)
-
-
 urlpatterns = [
     # path('', include(router.urls)),
     path('search/', views.Search, name='search'),
