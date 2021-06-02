afe5c4d4ea9f2d12e01e9169b7675cfd811ddc61
Gabryss <gabriel.garcia@utbm.fr>
2021-06-02 21:51:20 +0200

    Change pagination

--- API/settings.py
+++ API/settings.py
@@ -86,8 +86,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'API.wsgi.application'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
